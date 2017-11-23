a78dcc3bc7aec2811e5429b06b2b883c8612a901
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-11-23 17:05:04 +0800

    ch05 練習七: 路由命名

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subje
     return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
 }) -> where(['subject' => '(chinese|english|math)']);
 */
-
+/*
 //練習六: 路由群組->透過prefix前綴，將網址前套上student
 Route::pattern('student_no','s[0-9]{10}');
 Route::group(['prefix' => 'student'],function(){
@@ -65,4 +65,17 @@ Route::group(['prefix' => 'student'],function(){
     Route::get('{student_no}/score/{subject?}', function ($student_no, $subject = null) {
         return '學號：' . $student_no . '的' . ((is_null($subject)) ? '所有科目' : $subject) . '成績';
     })->where(['subject' => '(chinese | english | math)']);
+});
+*/
+//練習七: 路由命名
+Route::pattern('student_no','s[0-9]{10}');
+Route::group(['prefix' => 'student'],function(){
+    Route::get('{student_no}', ['as' => 'student', 'uses' => function ($student_no) {
+        return '學號：' . $student_no;
+    }
+    ]);
+    Route::get('{student_no}/score/{subject?}',['as' => 'student.score',
+        'uses' => function ($student_no, $subject = null) {
+            return '學號：' . $student_no . '的' . ((is_null($subject)) ? '所有科目' : $subject) . '成績';
+        }])->where(['subject' => '(chinese | english | math)']);
 });
