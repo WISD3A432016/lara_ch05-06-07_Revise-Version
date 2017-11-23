5e569d7a2831fe622166b90033995d74232808ff
Jing-Ru Sun <sun.3a432016@gmail.com>
2017-11-23 17:24:33 +0800

    ch06練習三(2) : 加入indexc函數

--- app/Http/Controllers/Cool/TestController.php
+++ app/Http/Controllers/Cool/TestController.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\Controller;
 
 class TestController extends Controller
 {
-    //
+    //ch06練習三(2): 加入indexc函數
+    public function indexc(){
+        return 'Cooler';
+    }
 }
